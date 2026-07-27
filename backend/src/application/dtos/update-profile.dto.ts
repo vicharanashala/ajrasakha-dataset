9c0c5e52115903c23633aeb23f75890e7ef66eb1
Abiram-k <abiramk0107@gmail.com>
2026-07-27 22:42:08 +0530

    feat(backend): add validation decorators to profile DTOs

--- backend/src/application/dtos/update-profile.dto.ts
+++ backend/src/application/dtos/update-profile.dto.ts
@@ -1,20 +1,39 @@
+import { IsString, IsOptional, IsEmail } from 'class-validator';
+
 export class UpdateProfileDto {
+  @IsOptional()
+  @IsString()
   firstName?: string;
+
+  @IsOptional()
+  @IsString()
   lastName?: string;
+
+  @IsOptional()
+  @IsString()
   state?: string;
 }
 
 export class ChangePasswordDto {
+  @IsString()
   currentPassword: string;
+
+  @IsString()
   newPassword: string;
 }
 
 export class RequestPasswordResetDto {
+  @IsEmail()
   email: string;
 }
 
 export class VerifyPasswordResetDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
   otp: string;
+
+  @IsString()
   newPassword: string;
 }
